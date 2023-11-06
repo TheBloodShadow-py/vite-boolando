2f952ad49694fd05acc97a7c633a2cab01ab2c41
Fabio <thebloodshadow14@gmail.com>
2023-11-06 16:03:33 +0100

    ADD - NavBar.vue

---
+++ src/components/Cards.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          name: "RELAXED FIT TEE UNISEX",
+          price: "14,99  €;",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<script>
+import logo from "/boolean-logo.png";
+export default {
+  data() {
+    return {
+      logo,
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <ul class="types">
+      <li>Donna</li>
+      <li>Uomo</li>
+      <li>Bambini</li>
+    </ul>
+    <img class="logo" alt="logo" :src="logo" />
+    <ul>
+      <li><img src="../assets/profile.svg" alt="profile icon" /></li>
+      <li><img src="../assets/heart.svg" alt="heart icon" /></li>
+      <li><img src="../assets/shopping-cart.svg" alt="shopping cart icon" /></li>
+    </ul>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "../partials/_varibles.scss";
+
+header {
+  background-color: $header-color;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 18px;
+  position: fixed;
+  left: 0;
+  width: 100%;
+  top: 0;
+  color: $header-text-color;
+  box-shadow: 0px 5px 5px rgba(17, 17, 17, 0.315);
+  font-family: "Dhurjati", sans-serif;
+  line-height: 1;
+  font-size: 22px;
+  z-index: 5;
+  .logo {
+    width: 120px;
+  }
+  ul {
+    display: flex;
+    gap: 15px;
+    li:hover {
+      cursor: pointer;
+    }
+  }
+}
+</style>
